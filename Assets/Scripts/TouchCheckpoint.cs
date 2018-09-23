a97dcbb63ebaac53496ad8fb482cee4765c838d8
Sergey Papushin <spapushin@gmail.com>
2018-09-23 03:26:53 -0500

    Make checkpoints invisible and never get destroyed

--- Assets/Scripts/TouchCheckpoint.cs
+++ Assets/Scripts/TouchCheckpoint.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TouchCheckpoint : MonoBehaviour {
-
+public class TouchCheckpoint : MonoBehaviour
+{
+    void Awake()
+    {
+        GetComponent<SpriteRenderer>().sprite = null;
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "Character")
         {
             other.GetComponent<CharacterControl>().SetCheckpoint(transform.position);
-            Destroy(gameObject);
         }
     }
 }
