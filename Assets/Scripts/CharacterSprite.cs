a2e9f27040b80f7e3ab0d72076e7920793f388a5
Sergey Papushin <spapushin@gmail.com>
2018-09-23 16:26:40 -0500

    Add aftershadow

--- Assets/Scripts/CharacterSprite.cs
+++ Assets/Scripts/CharacterSprite.cs
@@ -6,11 +6,25 @@ public class CharacterSprite : MonoBehaviour {
 
 	public List<Sprite> sprites;
 	public int curSprite;
+	public GameObject shadow;
 	
 	private SpriteRenderer spriteRenderer;
+	private float timeBetweenShadows = 0.05f;
+	private float timeRemaining;
 
 	void Start() {
 		spriteRenderer = GetComponent<SpriteRenderer>();
+		timeRemaining = timeBetweenShadows;
+	}
+
+	void Update() {
+		timeRemaining -= Time.deltaTime;
+		if (timeRemaining <= 0)
+		{
+			timeRemaining = timeBetweenShadows;
+			var newShadow = Instantiate(shadow, transform.position, Quaternion.identity);
+			newShadow.GetComponent<SpriteRenderer>().sprite = spriteRenderer.sprite;
+		}
 	}
 
 	public void SetSprite(int spriteNum) {
---
+++ Assets/Scripts/Shadow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour {
+	private SpriteRenderer sprite;
+	private float initialAlpha = 0.15f;
+	private float fadeRate = 1f;
+
+	void Awake () {
+		sprite = GetComponent<SpriteRenderer>();
+		transform.Translate(new Vector3(0, 0, 10));
+		setAlpha(initialAlpha);
+	}
+	
+	void Update () {
+		var newAlpha = Mathf.Max(sprite.color.a - Time.deltaTime * fadeRate, 0);
+		if (newAlpha == 0)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			setAlpha(newAlpha);
+		}
+	}
+
+	private void setAlpha(float newAlpha) {
+		sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, newAlpha);
+	}
+}
