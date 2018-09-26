06edc9ccecc644e2cc06b78d6a620b52c1e29679
Sergey Papushin <spapushin@gmail.com>
2018-09-26 16:14:00 -0500

    Only collect orb when character is grounded

--- Assets/Scripts/TouchOrb.cs
+++ Assets/Scripts/TouchOrb.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class TouchOrb : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    private bool collected;
+    private CharacterControl character;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        collected = false;
+        character = GameObject.Find("Character").GetComponent<CharacterControl>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (collected)
+        {
+            if (character.grounded)
+            {
+                character.GetComponent<GameControl>().CollectOrb();
+                Destroy(gameObject);
+            }
+            else if (character.deathRemaining > 0)
+            {
+                collected = false;
+            }
+            setAlpha(0.5f);
+        }
+        else
+            setAlpha(1f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "Character")
         {
-            other.GetComponent<GameControl>().CollectOrb();
-            Destroy(gameObject);
+            collected = true;
         }
+    }
+
+    private void setAlpha(float newAlpha)
+    {
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, newAlpha);
     }
 }
